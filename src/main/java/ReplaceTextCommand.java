92eba3baea2b47f6eb9caf62965e94b103d129ef
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2025-01-29 14:02:09 +0100

    Add ReplaceTextCommand and it test class as it was not added somehow

---
+++ src/main/java/ReplaceTextCommand.java
@@ -0,0 +1,52 @@
+/**
+ * <p>Represents a command that can be applied to a text.
+ * This command replaces a target string with a replacement string.</p>
+ *
+ * @author Johannes Nupen Theigen
+ * @version 0.0.1
+ * @since 01.29.2025
+ */
+public class ReplaceTextCommand implements TextCommand {
+  private final String target;
+  private final String replacement;
+
+  /**
+   * <p>Creates a new ReplaceTextCommand with the given target and replacement strings.</p>
+
+   * @param target the string to replace
+   * @param replacement the string that replaces the target
+   */
+  public ReplaceTextCommand(String target, String replacement) {
+    this.target = target;
+    this.replacement = replacement;
+  }
+
+  /**
+   * <p>Executes the command by replacing the target string with the replacement string.
+   * If the target string is not found, the text is returned unchanged.</p>
+
+   * @param text The text to apply the command to.
+   * @return The text with the target string replaced by the replacement string.
+   */
+  public String execute(String text) {
+    return text.replace(getTarget(), getReplacement());
+  }
+
+  /**
+   * <p>Retrieves the target String to be replaced.</p>
+
+   * @return The targeted part of the string to place.
+   */
+  public String getTarget() {
+    return target;
+  }
+
+  /**
+   * <p>Retrieves the string that is to replace the target string.</p>
+
+   * @return The string that replaces the target.
+   */
+  public String getReplacement() {
+    return replacement;
+  }
+}
