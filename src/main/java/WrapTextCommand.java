23ebecd902b671bf1f89ab3ae38b762be45c9c0a
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2025-01-29 13:59:00 +0100

    WrapTextCommand v-0.0.1 (Task 4)
    Add WrapTextCommand class and test class that tests its functionality.

---
+++ src/main/java/WrapTextCommand.java
@@ -0,0 +1,54 @@
+/**
+ * <p>Represents a command that can be applied to a text.
+ * This command wraps a text with an opening and end string.</p>
+
+ * @author Johannes Nupen Theigen
+ * @version 0.0.1
+ * @since 01.29.2025
+ */
+public class WrapTextCommand implements TextCommand {
+
+  private final String opening;
+  private final String end;
+
+  /**
+   * <p>Creates a new WrapTextCommand with the given opening and end strings.</p>
+
+   * @param opening The opening string.
+   * @param end The end string.
+   */
+  public WrapTextCommand(String opening, String end) {
+    this.opening = opening;
+    this.end = end;
+  }
+
+  /**
+   * <p>Wraps a text with opening and end strings.
+   * For example, if the opening is "H" and the end is "d!", the text "ello Worl"
+   * will be wrapped as "Hello World!".</p>
+
+   * @param text The text to apply the command to.
+   * @return The text wrapped with the opening and end strings.
+   */
+  public String execute(String text) {
+    return getOpening() + text + getEnd();
+  }
+
+  /**
+   * <p>Returns the opening string.</p>
+
+   * @return The opening string.
+   */
+  public String getOpening() {
+    return opening;
+  }
+
+  /**
+   * <p>Returns the end string.</p>
+
+   * @return The end string.
+   */
+  public String getEnd() {
+    return end;
+  }
+}
---
+++ src/test/java/WrapTextCommandTest.java
@@ -0,0 +1,84 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <p>Test class for the WrapTextCommand class to ensure that
+ * its methods execute, getOpening and getEnd work as expected.
+ * It provides both positive and negative tests that follows the AAA
+ * (Arrange, Act, Assert) pattern.</p>
+ */
+class WrapTextCommandTest {
+
+  /**
+   * <p>Positive test that assures that a text is properly
+   * wrapped with the opening and end strings.</p>
+   */
+  @Test
+  void executePositiveTest()
+  {
+    WrapTextCommand command = new WrapTextCommand("H", "d!");
+    String input = "ello Worl";
+    String result = command.execute(input);
+    assertEquals("Hello World!", result);
+  }
+
+  /**
+   * <p>Negative test that assures that a text is not incorrectly
+   * wrapped with the opening and end strings.</p>
+   */
+  @Test
+  void executeNegativeTest()
+  {
+    WrapTextCommand command = new WrapTextCommand("H", "d!");
+    String input = "ello Worl";
+    String result = command.execute(input);
+    assertNotEquals("HHello Worlddd!", result);
+  }
+
+  /**
+   * <p>Positive test that assures that the opening string is correctly
+   * retrieved from the wrap text command object.</p>
+   */
+  @Test
+  void getOpeningPositiveTest()
+  {
+    WrapTextCommand command = new WrapTextCommand("H", "d!");
+    String opening = command.getOpening();
+    assertEquals("H", opening);
+  }
+
+  /**
+   * <p>Negative test that assures that the opening string is not incorrectly
+   * retrieved from the wrap text command object.</p>
+   */
+  @Test
+  void getOpeningNegativeTest()
+  {
+    WrapTextCommand command = new WrapTextCommand("H", "d!");
+    String opening = command.getOpening();
+    assertNotEquals("HH", opening);
+  }
+
+  /**
+   * <p>Positive test that assures that the end string is correctly
+   * retrieved from the wrap text command object.</p>
+   */
+  @Test
+  void getEndPositiveTest() {
+    WrapTextCommand command = new WrapTextCommand("H", "d!");
+    String opening = command.getEnd();
+    assertEquals("d!", opening);
+  }
+
+  /**
+   * <p>Positive test that assures that the end string is correctly
+   * retrieved from the wrap text command object.</p>
+   */
+  @Test
+  void getEndNegativeTest() {
+    WrapTextCommand command = new WrapTextCommand("H", "d!");
+    String opening = command.getEnd();
+    assertNotEquals("d!!", opening);
+  }
+}
