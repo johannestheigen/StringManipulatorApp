e7cb2ade26c10f326d2bdbc32803bca11a19fa68
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2025-01-30 14:53:40 +0100

    CapitalizeTextCommand v-0.0.2 (Task 5)
    Remove static void method.

--- src/main/java/CapitalizeTextCommand.java
+++ src/main/java/CapitalizeTextCommand.java
@@ -3,7 +3,7 @@
  * which capitalizes the first letter of a text.</p>
 
  * @author Johannes Nupen Theigen
- * @version 0.0.2
+ * @version 0.0.3
  * @since 01.30.2025
  */
 public class CapitalizeTextCommand implements TextCommand {
@@ -19,11 +19,5 @@ public class CapitalizeTextCommand implements TextCommand {
       throw new IllegalArgumentException("Text cannot be null or empty.");
     }
     return text.replaceFirst(String.valueOf(text.charAt(0)), String.valueOf(text.charAt(0)).toUpperCase());
-  }
-
-  public static void main(String[] args) {
-    CapitalizeTextCommand capitalizeTextCommand = new CapitalizeTextCommand();
-    String textToBeCapitalized = "hello world";
-    System.out.println(capitalizeTextCommand.execute(textToBeCapitalized));
   }
 }
