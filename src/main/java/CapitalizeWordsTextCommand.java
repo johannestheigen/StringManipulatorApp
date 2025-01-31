1551f9398c2ea4a40c6e621c1e8b2f3f45993f9f
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2025-01-31 09:41:35 +0100

    CapitalizeWordsTextCommand v-0.0.1 (Task 5)
    Add CapitalizeWordsTextCommand and
    a test class that tests its execute() method.

---
+++ src/main/java/CapitalizeWordsTextCommand.java
@@ -0,0 +1,36 @@
+/**
+ * <p>
+ *   Represents a command that can be applied to a text
+ *   which capitalizes the first letter of each word in a text.
+ * </p>
+ *
+ * @author Johannes
+ * @version 0.0.1
+ * @since 01.31.2025
+ */
+public class CapitalizeWordsTextCommand extends CapitalizeTextCommand {
+
+  /**
+   * <p>Creates a new instance of the CapitalizeWordsTextCommand.</p>
+   */
+  public CapitalizeWordsTextCommand() {
+    super();
+  }
+
+  /**
+   * <p>Capitalizes the first letter of each word in a text.</p>
+
+   * @param text The text to apply the command to.
+   * @return The text with the first letter of each word capitalized.
+   */
+  @Override
+  public String execute(String text) {
+    if (text == null || text.isEmpty()) {
+      throw new IllegalArgumentException("Text cannot be null or empty.");
+    }
+    for (String word : text.split(" ")) {
+      text = text.replace(word, super.execute(word));
+    }
+    return text;
+  }
+}
---
+++ src/test/java/CapitalizeWordsTextCommandTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <p>Test class for the CapitalizeWordsTextCommand to ensure
+ * that its method execute works as expected. It provides both
+ * positive and negative tests that follows the AAA (Arrange,
+ * Act, Assert) pattern.</p>
+ */
+class CapitalizeWordsTextCommandTest {
+
+  /**
+   * <p>Positive test that assures that the first letter of each word
+   * in a string is capitalized.</p>
+   */
+  @Test
+  void executePositiveTest() {
+    CapitalizeWordsTextCommand capitalizeWordsTextCommand = new CapitalizeWordsTextCommand();
+    String result = capitalizeWordsTextCommand.execute("hello world");
+    String expected = "Hello World";
+    assertEquals(expected, result, "The execution output does not match the expected result, failing the positive test.");
+  }
+
+  /**
+   * <p>Negative test that assures that the first letter of each word
+   * in a string is not incorrectly capitalized.</p>
+   */
+  @Test
+  void executeNegativeTest() {
+    CapitalizeWordsTextCommand capitalizeWordsTextCommand = new CapitalizeWordsTextCommand();
+    String result = capitalizeWordsTextCommand.execute("hello world");
+    String expected = "hEllo woRld";
+    assertNotEquals(expected, result, "The execution output matches the expected result, failing the negative test.");
+  }
+}
