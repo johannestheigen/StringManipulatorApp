13aae1238209d4bf37785f7390aa142774f6fe8c
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2025-02-01 16:21:36 +0100

    Initial release

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,22 @@
+package app;
+
+/**
+ * <p>Starts the String Manipulator Application.</p>
+ *
+ * @author Johannes Nupen Theigen
+ * @version 0.0.1
+ * @since 02.01.2025
+ */
+public class Main
+{
+
+  /**
+   * <p>Runs the String Manipulator Application.</p>
+   * @param args The command line arguments.
+   */
+  public static void main(String[] args)
+  {
+    UserInterface ui = new UserInterface();
+    ui.start();
+  }
+}
